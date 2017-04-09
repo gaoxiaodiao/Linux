dae8050152a70055a4dfb3a8a33e875c8369a4b1
gaoxiaodiao <805888196@qq.com>
2017-04-09 20:17:41 +0800

    二叉树相关声明及实现(完成构造/析构/遍历)

--- DataStruct/BinaryTree/BinaryTree.hpp
+++ DataStruct/BinaryTree/BinaryTree.hpp
@@ -6,6 +6,8 @@
 ****************************************/
 #ifndef __BINARYTREE_H__
 #define __BINARYTREE_H__
+#define __DEBUG__
+#include<iostream>
 #include<stack>
 
 //二叉树节点声明
@@ -26,7 +28,7 @@ template<typename T>
 class BinaryTree{
 	typedef BinaryTreeNode<T> Node;
 public:
-	BinaryTree(T*,size_t sz,T &invalid);			//构造函数
+	BinaryTree(T*,size_t sz,const T&invalid);		//构造函数
 	~BinaryTree();									//析构函数
 	void PrevOrder();			//前序遍历
 	void PrevOrderNonR();		//前序遍历非递归
@@ -49,6 +51,7 @@ public:
 	//Node* BecomeList();							//将二叉搜索树转换为双向链表
 protected:
 	Node *_CreateTree(T*,size_t,const T&,size_t&);	//创建树
+	void _Destroy(Node*);							//销毁树
 	void _PrevOrder(Node*);							//前序遍历
 	void _InOrder(Node *);							//中序遍历
 	void _PostOrder(Node*);							//后序遍历
@@ -61,4 +64,158 @@ private:
 	Node *_root;
 };
 
+//二叉树相关实现
+
+//构造函数
+template<typename T>
+BinaryTree<T>::BinaryTree(T *arr,size_t sz,const T &invalid){
+	size_t index = 0;
+	_root = _CreateTree(arr,sz,invalid,index);
+}
+//创建二叉树
+template<typename T>
+typename BinaryTree<T>::Node *BinaryTree<T>::_CreateTree(T *arr,size_t sz,const T& invalid,size_t &index){
+	if(index<sz && arr[index]!=invalid){
+		Node *root = new Node(arr[index]);
+		root->_left = _CreateTree(arr,sz,invalid,++index);
+		root->_right = _CreateTree(arr,sz,invalid,++index);
+		return root;
+	}
+	return NULL;
+}
+//析构函数
+template<typename T>
+BinaryTree<T>::~BinaryTree(){
+#ifdef __DEBUG__
+	std::cout<<"DEBUG:检查析构:";
+#endif
+	_Destroy(_root);
+#ifdef __DEBUG__
+	std::cout<<std::endl;
+#endif
+}
+//销毁二叉树
+template<typename T>
+void BinaryTree<T>::_Destroy(Node *root){
+	if(root==NULL){
+		return ;
+	}
+	_Destroy(root->_left);
+	_Destroy(root->_right);
+#ifdef __DEBUG__
+	std::cout<<root->_value<<" ";
+#endif
+	delete root;
+}
+
+//前序遍历
+template<typename T>
+void BinaryTree<T>::PrevOrder(){
+	_PrevOrder(_root);
+	std::cout<<std::endl;
+}
+template<typename T>
+void BinaryTree<T>::_PrevOrder(Node *root){
+	if(root==NULL){
+		return ;
+	}
+	std::cout<<root->_value<<" ";
+	_PrevOrder(root->_left);
+	_PrevOrder(root->_right);
+}
+template<typename T>
+void BinaryTree<T>::PrevOrderNonR(){
+	std::stack<Node*> s;
+	Node *cur = _root;
+	while(cur!=NULL || !s.empty()){
+		
+		while(cur!=NULL){
+			s.push(cur);
+			std::cout<<cur->_value<<" ";
+			cur = cur->_left;
+		}
+		
+		Node *top = s.top();
+		s.pop();
+		
+		cur = top->_right;
+	}
+	std::cout<<std::endl;
+}
+//中序遍历
+template<typename T>
+void BinaryTree<T>::InOrder(){
+	_InOrder(_root);
+	std::cout<<std::endl;
+}
+template<typename T>
+void BinaryTree<T>::_InOrder(Node *root){
+	if(root==NULL){
+		return ;
+	}
+	_InOrder(root->_left);
+	std::cout<<root->_value<<" ";
+	_InOrder(root->_right);
+}
+template<typename T>
+void BinaryTree<T>::InOrderNonR(){
+	std::stack<Node*> s;
+	Node *cur = _root;
+	
+	while(cur!=NULL || !s.empty()){
+		
+		while(cur!=NULL){
+			s.push(cur);
+			cur = cur->_left;
+		}
+
+		Node *top = s.top();
+		s.pop();
+		std::cout<<top->_value<<" ";
+
+		cur = top->_right;
+	}
+
+	std::cout<<std::endl;
+}
+
+//后序遍历
+template<typename T>
+void BinaryTree<T>::PostOrder(){
+	_PostOrder(_root);
+	std::cout<<std::endl;
+}
+template<typename T>
+void BinaryTree<T>::_PostOrder(Node *root){
+	if(root==NULL){
+		return ;
+	}
+	_PostOrder(root->_left);
+	_PostOrder(root->_right);
+	std::cout<<root->_value<<" ";
+}
+template<typename T>
+void BinaryTree<T>::PostOrderNonR(){
+	std::stack<Node*> s;
+	Node* cur = _root;
+	Node *prev = NULL;
+	while(cur!=NULL || !s.empty()){
+		
+		while(cur!=NULL){
+			s.push(cur);
+			cur = cur->_left;
+		}
+
+		Node *top = s.top();
+		if(top->_right==NULL || top->_right == prev){
+			prev = top;
+			std::cout<<top->_value<<" ";
+			s.pop();
+		}else{
+			cur = top->_right;
+		}
+
+	}
+	std::cout<<std::endl;
+}
 #endif
