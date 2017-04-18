22ce237cea16b0125d35efb0a13791b4c6bf78c9
gaoxiaodiao <805888196@qq.com>
2017-04-18 11:29:12 +0800

    哈希表(线性探测法)完成

--- DataStruct/HashTable/HashTable.hpp
+++ DataStruct/HashTable/HashTable.hpp
@@ -5,6 +5,7 @@
 *开发环境:Kali Linux/g++ v6.3.0
 ****************************************/
 #include<vector>
+#include<cassert>
 //哈希表的三种状态
 enum STATUS{
 	EMPTY,		//当前节点为空
@@ -60,6 +61,7 @@ public:
 protected:
 	void _CheckCapacity();		//检查是否需要扩容,进行扩容操作
 	size_t _HashFun(const K &);	//使计算出来的下标处于合法位置
+	size_t _GetNewSize(size_t);	//从素数表中得出下一次扩容的大小
 private:
 	//禁止拷贝构造、赋值(懒人做法)
 	HashTable(const HashTable &);
@@ -117,3 +119,53 @@ std::pair<typename HashTable<K,V,HashFun>::Node*,bool>
 		//遇到空位置,该元素不存在
 		return std::make_pair(_tables[index],false);
 	}
+
+//////////////////哈希表扩容///////////////////
+template<typename K,typename V,typename HashFun>
+void HashTable<K,V,HashFun>::_CheckCapacity(){
+	if(_tables.empty()){
+		//哈希表为空
+		_tables.resize(_GetNewSize(_size));
+	}
+	//当负载因子大于7时,哈希冲突概率会增大,此时扩容
+	if(_size*10/_tables.size() > 7){
+		size_t newSize = _GetNewSize(_size);
+		//创建临时对象
+		HashTable<K,V,HashFun> tmp(newSize);
+		//将数据重新插入临时对象
+		for(size_t i=0; i<_tables.size(); ++i){
+			if(_tables[i]._status == EXIST){
+				//将有效元素插入tmp
+				tmp.Insert(_tables[i]._key,_tables[i]._value);
+			}
+		}
+		//交换
+		std::swap(_tables,tmp._tables);
+		std::swap(_size,tmp._size);
+	}
+}
+
+////////////////////哈希方法///////////////////
+template<typename K,typename V,typename HashFun>
+size_t HashTable<K,V,HashFun>::_HashFun(const K &key){
+	return HashFun()(key)%_size;
+}
+////////////////////素数表/////////////////////
+template<typename K,typename V,typename HashFun>
+size_t _GetNewSize(size_t size = 0){
+	const int _PrimeSize = 28;
+	static const unsigned long _PrimeList[_PrimeSize] = {
+		53ul,97ul,193ul,389ul,769ul,1543ul, 3079ul,
+		6151ul,12289ul,24593ul,49157ul,98317ul,196613ul,
+		393241ul,786433ul,1572869ul,3145739ul,6291469ul,
+		12582917ul,25165843ul,50331653ul,100663319ul,
+		201326611ul,402653189ul,805306457ul,1610612741ul,
+		3221225473ul,4294967291u
+	};
+	for(size_t i=0; i<_PrimeSize; ++i){
+		if( size <_PrimeList[i] ){
+			return _PrimeList[i];
+		}
+	}
+	assert(false);
+}
