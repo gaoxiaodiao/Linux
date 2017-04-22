58d47738219b7fad7a857875b50fac41b255ed6f
gaoxiaodiao <805888196@qq.com>
2017-04-22 16:12:53 +0800

    面试题05:从尾到头打印链表

--- AtOffer/Test5/2017-04-22.h
+++ AtOffer/Test5/2017-04-22.h
@@ -5,20 +5,23 @@
 *开发环境:Kali Linux/g++ v6.3.0
 ****************************************/
 #include<cassert>
-
 //链表节点定义
-typdef struct LinkNode{
+typedef struct LinkNode{
 	int _value;
 	LinkNode *_next;
+
+	LinkNode(const int value)
+		:_value(value)
+		,_next(NULL){}
 }Node;
 
 //将链表逆序
-void PrintfR(Node *head){
+void PrintR(Node *head){
 	if(head==NULL){
 		//当前节点为空,直接返回
 		return ;
 	}
 	//继续向后走
-	PrintfR(head->_next);
+	PrintR(head->_next);
 	std::cout<<head->_value<<" ";
 }
