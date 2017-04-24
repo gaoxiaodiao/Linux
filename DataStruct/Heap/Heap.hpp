23da2d132556c00d5454e18ab45e1d673ad57a21
gaoxiaodiao <805888196@qq.com>
2017-04-24 15:50:08 +0800

    堆相关声明及实现

--- DataStruct/Heap/Heap.hpp
+++ DataStruct/Heap/Heap.hpp
@@ -6,18 +6,22 @@
 ****************************************/
 #ifndef __HEAP_H__
 #define __HEAP_H__
+#include<iostream>
 #include<functional>
 #include<vector>
-template<typename T,typename Com = std::less<T>>
+template<typename T,typename Com = std::less<T> >
 class Heap{
 	public:
 		Heap(T arr[],int size);	//构造函数
 	public:
 		void Push(const T&);	//入数据
 		void Pop();				//出数据
+		void Print();			//打印堆(测试使用)
+		bool CheckIsHeap();		//判断是否为堆
 	protected:
 		void _AdjustUp(int);		//向上调整
 		void _AdjustDown(int);	//向下调整
+		bool _CheckIsHeap(int);	//递归判断是否为堆
 	private:
 		Heap(const Heap& );
 		Heap& operator=(const Heap&);
@@ -25,10 +29,18 @@ class Heap{
 		std::vector<T> _heap;
 };
 
+template<typename T,typename Com>
+void Heap<T,Com>::Print(){
+	for(int i=0; i<_heap.size(); ++i){
+		std::cout<<_heap[i]<<" ";
+	}
+	std::cout<<std::endl;
+}
+
 //构造函数:建堆
 //时间复杂度O(N*lgN)
-template<typename T,typename Com = std::less<T>>
-Heap::Heap(T arr[],int size)
+template<typename T,typename Com> 
+Heap<T,Com>::Heap(T arr[],int size)
 	:_heap(arr,arr+size){
 	//从最后一个非叶子节点开始,不断的向下调整
 	for(int i=(size-2)/2; i>=0; --i){
@@ -38,18 +50,18 @@ Heap::Heap(T arr[],int size)
 
 //向下调整
 //时间复杂度O(logN)
-template<typename T,typename Com = std::less<T>>
-void Heap::_AdjustDown(int index){
+template<typename T,typename Com>
+void Heap<T,Com>::_AdjustDown(int index){
 	int parent = index;
-	int child = parent*2;
+	int child = parent*2+1;
 	while(child<_heap.size()){
 		//找出大/小孩子
 		if(child+1<_heap.size() &&
-				Com(_heap[child],_heap[child+1])){
+				Com()(_heap[child+1],_heap[child])){
 			child++;
 		}
 		//找出根节点
-		if(Com(_heap[parent],_heap[child])){
+		if(Com()(_heap[child],_heap[parent])){
 			//当前根节点不满足条件,进行交换
 			//交换后,继续向下调整
 			std::swap(_heap[parent],_heap[child]);
@@ -64,18 +76,18 @@ void Heap::_AdjustDown(int index){
 
 //向上调整
 //时间复杂度O(lgN)
-template<typename T,typename Com = std::less<T>>
-void Heap::_AdjustUp(int index){
+template<typename T,typename Com>
+void Heap<T,Com>::_AdjustUp(int index){
 	int parent = index;
-	int child = parent*2;
+	int child = parent*2+1;
 	while(parent>=0){
 		//两个孩子中找较大/小的,进行交换
 		if(child+1<_heap.size() &&
-				Com(_heap[child+1],_heap[child])){
+				Com()(_heap[child+1],_heap[child])){
 			child++;
 		}
 		//孩子与父亲间找出较大/小的,进行交换
-		if(Com(_heap[parent],_heap[child])){
+		if(Com()(_heap[child],_heap[parent])){
 			//交换后,继续向上调整
 			std::swap(_heap[parent],_heap[child]);
 			child = parent;
@@ -88,17 +100,39 @@ void Heap::_AdjustUp(int index){
 }
 
 //插入
-template<typename T,typename Com = std::less<T>>
-void Heap::Push(const T& e){
+template<typename T,typename Com>
+void Heap<T,Com>::Push(const T& e){
 	_heap.push_back(e);
 	_AdjustUp((_heap.size()-2)/2);
 }
 
 //删除
-template<typename T,typename Com = std::less<T>>
-void Heap::Pop(){
+template<typename T,typename Com>
+void Heap<T,Com>::Pop(){
 	std::swap(_heap[0],_heap[_heap.size()-1]);
 	_AdjustDown(0);
+	_heap.pop_back();
 }
 
+//判断是是否为堆
+template<typename T,typename Com>
+bool Heap<T,Com>::CheckIsHeap(){
+	return _CheckIsHeap(0);
+}
+
+template<typename T,typename Com>
+bool Heap<T,Com>::_CheckIsHeap(int index){
+	if(index>_heap.size()){
+		return true;
+	}
+	int parent = index;
+	int child = parent*2+1;
+	Com compare;
+	if(compare(_heap[parent],_heap[child])&&
+	(child+1>_heap.size() ||
+	 compare(_heap[parent],_heap[child+1])))
+		return _CheckIsHeap(child)&&_CheckIsHeap(child+1);
+	}
+	return false;
+}
 #endif
