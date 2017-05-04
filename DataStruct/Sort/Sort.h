c36ac53ddb1ac08548e744f56892dd2fb371a325
gaoxiaodiao <805888196@qq.com>
2017-05-04 15:42:50 +0800

    快速排序、希尔排序、堆排序

--- DataStruct/Sort/Sort.h
+++ DataStruct/Sort/Sort.h
@@ -4,6 +4,7 @@
 *创建时间:2017年05月03日 星期三 17时53分29秒
 *开发环境:Kali Linux/g++ v6.3.0
 ****************************************/
+//冒泡排序优化
 void BubbleSort(int arr[],int size){
 	bool flag = true;
 	for(int i=0; flag; ++i){
@@ -18,14 +19,40 @@ void BubbleSort(int arr[],int size){
 		}
 	}
 }
+//快速排序
+void QuickSort(int arr[],int left,int right){
+	if(left>=right){
+		return ;
+	}
+	int low = left;
+	int high = right;
+	int key = right;
+	while(low<high){
+		while(low<high && arr[low]<=arr[key]){
+			++low;
+		}
+		while(low<high && arr[high]>=arr[key]){
+			--high;
+		}
+		int tmp = arr[low];
+		arr[low] = arr[high];
+		arr[high] = tmp;
+	}
+	int tmp = arr[low];
+	arr[low] = arr[key];
+	arr[key] = tmp;
+	QuickSort(arr,left,low-1);
+	QuickSort(arr,low+1,right);
+}
 
+//选择排序优化
 void SelectSort(int arr[],int size){
 	for(int i=0; i<size-i;++i){
 		int minPos = i;
 		int min = arr[i];
 		int maxPos = size-i-1;
 		int max = arr[size-i-1];
-		for(int j=i+1; j<size-i; ++j){
+		for(int j=i; j<size-i; ++j){
 			if(arr[minPos] > arr[j]){
 				minPos = j;
 				min = arr[j];
@@ -45,7 +72,43 @@ void SelectSort(int arr[],int size){
 		arr[size-i-1] = max;
 	}
 }
+//向下调整算法
+void AdjustDown(int arr[],int size,int pos){
+	int parent = pos;
+	int child = pos*2+1;
+	while(child<size){
+		if(child+1<size && arr[child+1] > arr[child]){
+			child++;
+		}
+		if(arr[child] > arr[parent]){
+			int tmp = arr[child];
+			arr[child] = arr[parent];
+			arr[parent] = tmp;
 
+			parent = child;
+			child = parent*2+1;
+		}else{
+			break;
+		}
+	}
+}
+
+//堆排序
+void HeapSort(int arr[],int size){
+	//建堆
+	for(int i=(size-2)/2; i>=0; --i){
+		AdjustDown(arr,size,i);
+	}
+	//选择排序
+	for(int i=0; i<size; ++i){
+		int tmp = arr[0];
+		arr[0] = arr[size-i-1];
+		arr[size-i-1] = tmp;
+		AdjustDown(arr,size-i-1,0);
+	}
+}
+
+//插入排序
 void InsertSort(int arr[],int size){
 	for(int i=1;i<size;++i){
 		int end = i-1;
@@ -55,5 +118,22 @@ void InsertSort(int arr[],int size){
 			end--;
 		}
 		arr[end+1] = key;
+	}
+}
+
+//shell排序
+void ShellSort(int arr[],int size){
+	int gap = size;
+	while(gap>1){
+		gap = gap/3+1;
+		for(int i=gap; i<size; i++){
+			int end = i-gap;
+			int key = arr[i];
+			while(end>=0 && arr[end]>key){
+				arr[end+gap] = arr[end];
+				end -= gap;
+			}
+			arr[end+gap] = key;
+		}
 	}
 }
